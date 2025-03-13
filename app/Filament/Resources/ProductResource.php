37e6ea8ca0c2446751426cff4fb8e5a87160295a
Khalifeh Ahmad <khalifeh92ahmed@hotmail.com>
2025-03-13 12:58:53 -0800

    products resources

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Enums\ProductStatusEnum;
+use App\Filament\Resources\ProductResource\Pages;
+use App\Filament\Resources\ProductResource\Pages\EditProduct;
+use App\Filament\Resources\ProductResource\Pages\ProductImages;
+use App\Filament\Resources\ProductResource\RelationManagers;
+use App\Models\Department;
+use App\Models\Product;
+use Filament\Forms;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Pages\SubNavigationPosition;
+use Filament\Resources\Pages\Page;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+class ProductResource extends Resource
+{
+  protected static ?string $model = Product::class;
+
+  protected static ?string $navigationIcon = 'heroicon-s-queue-list';
+
+  protected static SubNavigationPosition $subNavigationPosition = SubNavigationPosition::End;
+
+  public static function form(Form $form): Form
+  {
+    return $form
+      ->schema([
+        Grid::make()
+          ->schema([
+            TextInput::make('title')->live(onBlur: true)->required()
+              ->afterStateUpdated(function (string $operation, $state, callable $set) {
+                $set('slug', Str::slug($state));
+              }),
+            TextInput::make('slug')->required(),
+            Select::make('department_id')->relationship('department', 'name')
+              ->label(__('Department'))->preload()->searchable()->required()->reactive()
+              ->afterStateUpdated(function (callable $set) {
+                $set('category_id', null);
+              }),
+            Select::make('category_id')->relationship(
+              'category',
+              'name',
+              modifyQueryUsing: function (Builder $query, callable $get) {
+                $departmentId = $get('department_id');
+                if ($departmentId) {
+                  $query->where('department_id', $departmentId);
+                }
+              }
+            )->label(__('Category'))->preload()->searchable()->required()
+
+          ]),
+        RichEditor::make('description')->required()->toolbarButtons([
+          'blockquote',
+          'blod',
+          'bulletList',
+          'h2',
+          'h3',
+          'italic',
+          'link',
+          'orderedList',
+          'redo',
+          'strike',
+          'underLine',
+          'undo',
+          'table'
+        ])
+          ->columnSpan(2),
+        TextInput::make('price')->required()->numeric(),
+        TextInput::make('quantity')->integer(),
+        Select::make('status')->options(ProductStatusEnum::lables())
+          ->default(ProductStatusEnum::Draft->value)->required()
+
+      ]);
+  }
+
+  public static function table(Table $table): Table
+  {
+    return $table
+      ->columns([
+        SpatieMediaLibraryImageColumn::make('imgaes')->collection('images')->limit(1)
+          ->conversion('thumb')->label('Image'),
+        TextColumn::make('title')->sortable()->words(10)->searchable(),
+        TextColumn::make('status')->badge()->colors(ProductStatusEnum::colors()),
+        TextColumn::make('department.name'),
+        TextColumn::make('category.name'),
+        TextColumn::make('created_at')->dateTime(),
+      ])
+      ->filters([
+        SelectFilter::make('status')->options(ProductStatusEnum::lables()),
+        SelectFilter::make('department_id')
+          ->relationship('department', 'name')
+      ])
+      ->actions([
+        Tables\Actions\EditAction::make(),
+      ])
+      ->bulkActions([
+        Tables\Actions\BulkActionGroup::make([
+          Tables\Actions\DeleteBulkAction::make(),
+        ]),
+      ]);
+  }
+
+  public static function getRelations(): array
+  {
+    return [
+      //
+    ];
+  }
+
+  public static function getPages(): array
+  {
+    return [
+      'index' => Pages\ListProducts::route('/'),
+      'create' => Pages\CreateProduct::route('/create'),
+      'edit' => Pages\EditProduct::route('/{record}/edit'),
+      'images' => Pages\ProductImages::route('/{record}/images')
+    ];
+  }
+
+  public static function getRecordSubNavigation(Page $page): array
+  {
+    return
+      $page->generateNavigationItems([
+        EditProduct::class,
+        ProductImages::class
+      ]);
+  }
+}
